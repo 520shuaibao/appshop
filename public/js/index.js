4f4e700d86671899a9baa2a31192cab0b2606907
zhaoxuewen5306 <937669694@qq.com>
2016-12-20 13:06:19 +0800

    Add files via upload

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+//(function (win,doc){
+//    function change()
+//    {
+//         document.documentElement.style.fontSize=20/1349*document.
+//                documentElement.clientWidth+'px';
+//    }
+//    doc.addEventListener('DOMContentLoaded',change,false);
+//    win.addEventListener('resize',change,false);
+//})(window,document);
